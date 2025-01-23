b547922f07b7cc1cd74f6ccad729c7df6100f7eb
Amarjith P <72960252+amarjithp@users.noreply.github.com>
2025-01-23 19:27:31 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fitnest/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const HomePage(),
+    );
+  }
+}
